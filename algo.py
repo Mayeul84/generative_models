e97f640259590e57d91961e16a4e4982cc91333c
Mayeul84 <mayeul.lambert84140@gmail.com>
2026-03-27 18:32:25 +0100

    Changes

--- algo.py
+++ algo.py
@@ -107,11 +107,12 @@ def PNP_SGS(ro, MCMC_steps, x_true, y, Burn_in_steps, diffusing_model, operator,
                 
                 ro_min = 0.04
                 if ro < ro_min:
-                    t_end = inverse_variance_function(ro_min,model=diffusing_model)
+                    t_end = inverse_variance_function(np.sqrt(sigma_estimated**2 - ro_min**2),model=diffusing_model)
                 else:
-                    t_end = inverse_variance_function(ro,model=diffusing_model)
+                    t_end = inverse_variance_function(np.sqrt(sigma_estimated**2 - ro**2),model=diffusing_model)
 
-                print(f"t_star: {t_star} and t_end: {t_end}")
+                if show:
+                    print(f"\nt_star: {t_star} and t_end: {t_end}.   ")
 
             if show:
                 #print(f"noise level estimated = {noise_level}")
